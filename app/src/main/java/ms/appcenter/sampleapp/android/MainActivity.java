3ac653cee3f5e3b96809fd0390792d18352b3471
Renan Silva De Araujo <renan.araujo.ext@e-ai.com.br>
2022-03-08 13:59:15 -0300

    Subiida de key

--- app/src/main/java/ms/appcenter/sampleapp/android/MainActivity.java
+++ app/src/main/java/ms/appcenter/sampleapp/android/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
                     Analytics.class, Crashes.class, Distribute.class);
         } else {
             // Otherwise use the hardcoded string value here
-            AppCenter.start(getApplication(), "<APP SECRET HERE>",
+            AppCenter.start(getApplication(), "343a1ea8-14fc-415a-ae9e-b9fa0cf0941f",
                     Analytics.class, Crashes.class, Distribute.class);
         }
 
